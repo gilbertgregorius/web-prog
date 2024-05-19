6467a1c8ae58c6814f6b6c5ab42d6319931a4069
Gilbert Gregorius <gilbertgregorius@gmail.com>
2024-05-19 16:34:12 +0700

    test

--- PHP/index.php
+++ PHP/index.php
@@ -42,7 +42,7 @@ Jonathan Febrian Handoyo - 222102303
             <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#learn-more">About</a>
@@ -106,9 +106,6 @@ Jonathan Febrian Handoyo - 222102303
                         echo '<li class="nav-item">
                                 <a class="nav-link" href="Sign\sign_in_user_form.html" rel="nofollow">
                                     <i class="fa fa-user" aria-hidden="true"></i>
-                                </a>
-                                <a class="nav-link" href="Sign\sign_in_user_form.html" rel="nofollow">
-                                    <b> Login </b>
                                 </a>
                               </li>';
                     }
